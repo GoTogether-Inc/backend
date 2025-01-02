07b09b8d277d312ed5d423d27001e7f6510e1f24
MJJ <m3k0813@gmail.com>
2025-01-03 00:13:10 +0900

    fix: HostChannel 이름 필드 Unique 로 수정

--- src/main/java/com/gotogether/domain/hostchannel/entity/HostChannel.java
+++ src/main/java/com/gotogether/domain/hostchannel/entity/HostChannel.java
@@ -30,7 +30,7 @@ public class HostChannel extends BaseEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Column(name = "name", nullable = false)
+	@Column(name = "name", nullable = false, unique = true)
 	private String name;
 
 	@Column(name = "email", nullable = false)
