e502d73d63fe945abb0f88994d22ed93f44be765
MJJ <m3k0813@gmail.com>
2025-01-02 23:35:17 +0900

    test: HostChannelController 테스트 코드 작성

--- src/main/java/com/gotogether/domain/hostchannel/dto/request/HostChannelRequestDTO.java
+++ src/main/java/com/gotogether/domain/hostchannel/dto/request/HostChannelRequestDTO.java
@@ -3,9 +3,11 @@ package com.gotogether.domain.hostchannel.dto.request;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class HostChannelRequestDTO {
 	@JsonProperty("profileImageUrl")
