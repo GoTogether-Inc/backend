ce63b00722203aa1f495c4e005020e41ee3a0a1a
JJM <m3k0813@gmail.com>
2025-04-01 23:05:20 +0900

    fix: 이벤트 리스트 응답 관련 컨버터 수정

--- src/main/java/com/gotogether/domain/order/converter/OrderConverter.java
+++ src/main/java/com/gotogether/domain/order/converter/OrderConverter.java
@@ -1,11 +1,10 @@
 package com.gotogether.domain.order.converter;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Component;
 
+import com.gotogether.domain.event.dto.response.EventListResponseDTO;
 import com.gotogether.domain.event.entity.Event;
 import com.gotogether.domain.hashtag.entity.Hashtag;
 import com.gotogether.domain.order.dto.response.OrderDetailResponseDTO;
@@ -14,6 +13,8 @@ import com.gotogether.domain.order.entity.Order;
 import com.gotogether.domain.order.entity.OrderStatus;
 import com.gotogether.domain.ticket.entity.Ticket;
 import com.gotogether.domain.user.entity.User;
+import com.gotogether.global.util.DateFormatterUtil;
+import com.gotogether.global.util.DateUtil;
 
 @Component
 public class OrderConverter {
@@ -27,44 +28,36 @@ public class OrderConverter {
 	}
 
 	public static OrderedTicketResponseDTO toOrderedTicketResponseDTO(Order order) {
+
+		Event event = order.getTicket().getEvent();
+
+		EventListResponseDTO eventListDTO = EventListResponseDTO.builder()
+			.id(event.getId())
+			.bannerImageUrl(event.getBannerImageUrl())
+			.title(event.getTitle())
+			.hostChannelName(event.getHostChannel().getName())
+			.startDate(DateFormatterUtil.formatDate(event.getStartDate()))
+			.address(event.getAddress())
+			.onlineType(String.valueOf(event.getOnlineType()))
+
+			.hashtags(event.getHashtags().stream()
+				.map(Hashtag::getName)
+				.toList()
+			)
+
+			.remainDays(DateUtil.getDdayStatus(
+				LocalDate.from(event.getStartDate()),
+				LocalDate.from(event.getEndDate())))
+			.build();
+
 		return OrderedTicketResponseDTO.builder()
 			.id(order.getId())
-			.eventId(order.getTicket().getEvent().getId())
-			.bannerImageUrl(order.getTicket().getEvent().getBannerImageUrl())
-			.title(order.getTicket().getEvent().getTitle())
-			.hostChannelName(order.getTicket().getEvent().getHostChannel().getName())
-			.startDate(order.getTicket().getEvent().getStartDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")))
-			.address(order.getTicket().getEvent().getAddress())
-
-			.hashtags(order.getTicket().getEvent().getHashtags().stream()
-				.map(Hashtag::getName)
-				.collect(Collectors.toList()))
-
+			.event(eventListDTO)
 			.ticketQrCode(order.getTicketQrCode().getQrCodeImageUrl())
 			.ticketName(order.getTicket().getName())
 			.orderStatus(order.getStatus().name())
 			.isCheckIn(order.getTicketQrCode().getStatus().isCheckIn())
-
-			.remainDays(getDdayStatus(
-				LocalDate.from(order.getTicket().getEvent().getStartDate()),
-				LocalDate.from(order.getTicket().getEvent().getEndDate())))
 			.build();
-	}
-
-	private static String getDdayStatus(LocalDate startDate, LocalDate endDate) {
-		LocalDate today = LocalDate.now();
-		long remainDays = startDate.toEpochDay() - today.toEpochDay();
-		long remainDaysEnd = endDate.toEpochDay() - today.toEpochDay();
-
-		if (remainDays > 7) {
-			return "false";
-		} else if (remainDays > 0) {
-			return "D-" + remainDays;
-		} else if (remainDays <= 0 && remainDaysEnd >= 0) {
-			return "진행중";
-		} else {
-			return "종료";
-		}
 	}
 
 	public static OrderDetailResponseDTO toOrderedDetailResponseDTO(
@@ -74,12 +67,13 @@ public class OrderConverter {
 			.ticketQrCode(order.getTicketQrCode().getQrCodeImageUrl())
 			.title(event.getTitle())
 			.hostChannelName(event.getHostChannel().getName())
-			.startDate(event.getStartDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")))
+			.startDate(DateFormatterUtil.formatDate(event.getStartDate()))
 			.eventAddress(event.getAddress())
 			.ticketName(order.getTicket().getName())
 			.ticketPrice(order.getTicket().getPrice())
 			.orderStatus(order.getTicketQrCode().getStatus().name())
-			.remainDays(getDdayStatus(
+
+			.remainDays(DateUtil.getDdayStatus(
 				LocalDate.from(event.getStartDate()),
 				LocalDate.from(event.getEndDate())))
 			.build();
