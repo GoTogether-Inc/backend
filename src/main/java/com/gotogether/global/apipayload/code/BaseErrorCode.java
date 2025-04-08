dd4b232da4e9038b26467d3a2c296f2e29b4e65f
JJM <m3k0813@gmail.com>
2025-04-08 16:50:07 +0900

    fix: 사용안하는 메서드 제거

--- src/main/java/com/gotogether/global/apipayload/code/BaseErrorCode.java
+++ src/main/java/com/gotogether/global/apipayload/code/BaseErrorCode.java
@@ -1,6 +1,5 @@
 package com.gotogether.global.apipayload.code;
 
 public interface BaseErrorCode {
-	ErrorReasonDto getReason();
-	ErrorReasonDto getReasonHttpStatus();
+	ErrorReasonDTO getReasonHttpStatus();
 }
