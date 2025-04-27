a60bf6e2b6478112595d1ef84d85bee211e6d878
JJM <m3k0813@gmail.com>
2025-04-27 18:32:39 +0900

    feat: user 전화번호 형식 검사 추가

--- src/main/java/com/gotogether/domain/user/dto/request/UserRequestDTO.java
+++ src/main/java/com/gotogether/domain/user/dto/request/UserRequestDTO.java
@@ -3,13 +3,14 @@ package com.gotogether.domain.user.dto.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class UserRequestDTO {
-	
+
 	@NotNull(message = "사용자 ID는 필수입니다.")
 	private Long id;
 
@@ -17,6 +18,7 @@ public class UserRequestDTO {
 	private String name;
 
 	@NotBlank(message = "전화번호는 필수입니다.")
+	@Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "올바른 전화번호 형식이어야 합니다.")
 	private String phoneNumber;
 
 	@NotBlank(message = "이메일은 필수입니다.")
