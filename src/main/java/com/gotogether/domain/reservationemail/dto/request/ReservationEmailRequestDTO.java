f0c9ac4d7f3abef6cf1326a743ea5b347fb3c9aa
JJM <m3k0813@gmail.com>
2025-05-01 00:40:54 +0900

    fix: reservationDate Validation 메세지 수정

--- src/main/java/com/gotogether/domain/reservationemail/dto/request/ReservationEmailRequestDTO.java
+++ src/main/java/com/gotogether/domain/reservationemail/dto/request/ReservationEmailRequestDTO.java
@@ -29,6 +29,6 @@ public class ReservationEmailRequestDTO {
 	private String content;
 
 	@NotNull(message = "예약 일시는 필수입니다.")
-	@FutureOrPresent(message = "예약 날짜는 현재 시간 이후여야 합니다.")
+	@FutureOrPresent(message = "예약 일시는 현재 시간 이후여야 합니다.")
 	private LocalDateTime reservationDate;
 }
