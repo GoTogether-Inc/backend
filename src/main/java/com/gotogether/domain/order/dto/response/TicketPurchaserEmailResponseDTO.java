d881fd7bcf5caa9a23664ceb3897c22c5c095db7
희은 <datakubit2@gmail.com>
2025-05-03 00:06:04 +0900

    chore: 디자인 패턴 변경 고려 주석

--- src/main/java/com/gotogether/domain/order/dto/response/TicketPurchaserEmailResponseDTO.java
+++ src/main/java/com/gotogether/domain/order/dto/response/TicketPurchaserEmailResponseDTO.java
@@ -3,6 +3,9 @@ package com.gotogether.domain.order.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+/**
+ * 디자인 패턴 변경 고려
+ */
 @Getter
 @AllArgsConstructor
 public class TicketPurchaserEmailResponseDTO {
