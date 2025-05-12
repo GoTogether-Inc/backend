dc26134fdc905f4fb6eb538ec5914e9d618fd8aa
JJM <m3k0813@gmail.com>
2025-05-12 21:45:31 +0900

    test: HostChannelController 테스트 코드 수정

--- src/main/java/com/gotogether/domain/hostchannel/dto/request/OrderStatusRequestDTO.java
+++ src/main/java/com/gotogether/domain/hostchannel/dto/request/OrderStatusRequestDTO.java
@@ -1,9 +1,15 @@
 package com.gotogether.domain.hostchannel.dto.request;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderStatusRequestDTO {
 
 	@NotNull(message = "orderId는 필수입니다.")
