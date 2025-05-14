43f52b434e281188dc048bcc15b880eae8dfaf21
JJM <m3k0813@gmail.com>
2025-05-14 16:15:55 +0900

    feat: 특정 유저가 구매한 티켓에 대한 주문 메서드 구현

--- src/main/java/com/gotogether/domain/order/repository/OrderCustomRepository.java
+++ src/main/java/com/gotogether/domain/order/repository/OrderCustomRepository.java
@@ -6,8 +6,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import com.gotogether.domain.order.entity.Order;
+import com.gotogether.domain.user.entity.User;
 
 public interface OrderCustomRepository {
 
 	Page<Order> findByTicketIdsAndStatus(List<Long> ticketIds, String tags, Pageable pageable);
+
+	Page<Order> findByUser(User user, Pageable pageable);
 }
--- src/main/java/com/gotogether/domain/order/repository/OrderCustomRepositoryImpl.java
+++ src/main/java/com/gotogether/domain/order/repository/OrderCustomRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.gotogether.domain.order.repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
@@ -7,12 +8,17 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
+import com.gotogether.domain.event.entity.QEvent;
+import com.gotogether.domain.eventhashtag.entity.QEventHashtag;
+import com.gotogether.domain.hashtag.entity.QHashtag;
+import com.gotogether.domain.hostchannel.entity.QHostChannel;
 import com.gotogether.domain.order.entity.Order;
 import com.gotogether.domain.order.entity.OrderStatus;
 import com.gotogether.domain.order.entity.QOrder;
 import com.gotogether.domain.ticket.entity.QTicket;
 import com.gotogether.domain.ticketqrcode.entity.QTicketQrCode;
 import com.gotogether.domain.user.entity.QUser;
+import com.gotogether.domain.user.entity.User;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
@@ -56,6 +62,52 @@ public class OrderCustomRepositoryImpl implements OrderCustomRepository {
 			.select(order.count())
 			.from(order)
 			.where(baseCondition)
+			.fetchOne();
+
+		return new PageImpl<>(orders, pageable, totalCount);
+	}
+
+	@Override
+	public Page<Order> findByUser(User user, Pageable pageable) {
+		QOrder order = QOrder.order;
+		QTicket ticket = QTicket.ticket;
+		QTicketQrCode ticketQrCode = QTicketQrCode.ticketQrCode;
+		QUser qUser = QUser.user;
+		QEvent event = QEvent.event;
+		QHostChannel hostChannel = QHostChannel.hostChannel;
+		QEventHashtag eventHashtag = QEventHashtag.eventHashtag;
+		QHashtag hashtag = QHashtag.hashtag;
+
+		List<Order> orders = queryFactory
+			.selectDistinct(order)
+			.from(order)
+			.join(order.user, qUser)
+			.join(order.ticket, ticket).fetchJoin()
+			.join(ticket.event, event).fetchJoin()
+			.join(event.hostChannel, hostChannel).fetchJoin()
+			.join(order.ticketQrCode, ticketQrCode).fetchJoin()
+			.leftJoin(event.eventHashtags, eventHashtag).fetchJoin()
+			.leftJoin(eventHashtag.hashtag, hashtag).fetchJoin()
+			.where(
+				order.user.eq(user),
+				event.startDate.goe(LocalDateTime.now()),
+				order.status.ne(OrderStatus.CANCELED)
+			)
+			.offset(pageable.getOffset())
+			.limit(pageable.getPageSize())
+			.orderBy(order.createdAt.desc())
+			.fetch();
+
+		long totalCount = queryFactory
+			.select(order.countDistinct())
+			.from(order)
+			.join(order.ticket, ticket)
+			.join(ticket.event, event)
+			.where(
+				order.user.eq(user),
+				event.startDate.goe(LocalDateTime.now()),
+				order.status.ne(OrderStatus.CANCELED)
+			)
 			.fetchOne();
 
 		return new PageImpl<>(orders, pageable, totalCount);
