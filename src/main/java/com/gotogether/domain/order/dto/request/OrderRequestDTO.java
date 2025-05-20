cf44dc34335610f06757b611511708c2ab18a7df
JJM <m3k0813@gmail.com>
2025-05-21 01:08:30 +0900

    fix: 최대 티켓 주문 개수 유효성 검사 추가

--- src/main/java/com/gotogether/domain/order/dto/request/OrderRequestDTO.java
+++ src/main/java/com/gotogether/domain/order/dto/request/OrderRequestDTO.java
@@ -1,7 +1,8 @@
 package com.gotogether.domain.order.dto.request;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -15,6 +16,7 @@ public class OrderRequestDTO {
 	@NotNull(message = "eventId는 필수입니다.")
 	private Long eventId;
 
-	@Positive(message = "티켓 개수는 1 이상이어야 합니다.")
+	@Min(value = 1, message = "티켓 개수는 최소 1장 이상이어야 합니다.")
+	@Max(value = 4, message = "티켓 개수는 최대 4장까지만 구매할 수 있습니다.")
 	private int ticketCnt;
 }
