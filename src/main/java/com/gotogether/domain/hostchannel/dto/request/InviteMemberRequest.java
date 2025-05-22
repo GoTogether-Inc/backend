3e4004f68d742f7ee37e81929f0b7ec0576ca922
JJM <m3k0813@gmail.com>
2025-05-22 15:35:27 +0900

    fix: 호스트 멤버 초대 RequestBody 로 수정

---
+++ src/main/java/com/gotogether/domain/hostchannel/dto/request/InviteMemberRequest.java
@@ -0,0 +1,13 @@
+package com.gotogether.domain.hostchannel.dto.request;
+
+import jakarta.validation.constraints.Email;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class InviteMemberRequest {
+
+	@Email(message = "올바른 이메일 형식이어야 합니다.")
+	private String email;
+}
