86cce3332648065e9b402359e63d15ce39f44d93
JJM <m3k0813@gmail.com>
2025-06-07 01:16:52 +0900

    fix: 티켓 구매 가능 기간 검증 로직 추가

--- src/main/java/com/gotogether/global/apipayload/code/status/ErrorStatus.java
+++ src/main/java/com/gotogether/global/apipayload/code/status/ErrorStatus.java
@@ -26,16 +26,15 @@ public enum ErrorStatus implements BaseErrorCode {
 	_HASHTAG_EXIST(HttpStatus.BAD_REQUEST, "HASHTAG4002", "해시태그가 이미 존재합니다."),
 
 	_HOST_CHANNEL_NOT_FOUND(HttpStatus.NOT_FOUND, "HOST_CHANNEL4001", "호스트 채널이 없습니다."),
-	_HOST_CHANNEL_DELETE_FAILED_MEMBERS_EXIST(HttpStatus.BAD_REQUEST, "HOST_CHANNEL4002",
-		"멤버가 아직 존재하여, 호스트 채널을 삭제할 수 없습니다."),
+	_HOST_CHANNEL_DELETE_FAILED_MEMBERS_EXIST(HttpStatus.BAD_REQUEST, "HOST_CHANNEL4002", "멤버가 아직 존재하여, 호스트 채널을 삭제할 수 없습니다."),
 	_HOST_CHANNEL_MEMBER_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "HOST_CHANNEL4003", "호스트 채널에 이미 존재하는 멤버 입니다."),
 	_HOST_CHANNEL_EXISTS(HttpStatus.BAD_REQUEST, "HOST_CHANNEL4004", "호스트 채널이 이미 존재합니다."),
-	_HOST_CHANNEL_DELETE_FAILED_EVENTS_EXIST(HttpStatus.BAD_REQUEST, "HOST_CHANNEL4005",
-		"이벤트가 아직 존재하여, 호스트 채널을 삭제할 수 없습니다."),
+	_HOST_CHANNEL_DELETE_FAILED_EVENTS_EXIST(HttpStatus.BAD_REQUEST, "HOST_CHANNEL4005", "이벤트가 아직 존재하여, 호스트 채널을 삭제할 수 없습니다."),
 
 	_TICKET_NOT_FOUND(HttpStatus.NOT_FOUND, "TICKET4001", "티켓이 없습니다."),
 	_TICKET_NOT_ENOUGH(HttpStatus.BAD_REQUEST, "TICKET4002", "남은 티켓 수량이 부족합니다."),
 	_TICKET_ALREADY_CLOSED(HttpStatus.BAD_REQUEST, "TICKET4003", "이미 종료된 티켓입니다."),
+	_TICKET_SALE_UNAVAILABLE(HttpStatus.BAD_REQUEST, "TICKET4004", "현재 판매 중인 티켓이 아닙니다."),
 
 	_QR_CODE_ALREADY_USED(HttpStatus.BAD_REQUEST, "QR_CODE4001", "이미 사용된 QR 코드입니다."),
 	_INVALID_QR_CODE_FORMAT(HttpStatus.BAD_REQUEST, "QR_CODE4002", "잘못된 QR 코드 형식입니다."),
