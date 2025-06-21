29f951149fd04c51c097039a5e2a7afefaf71bd1
JJM <m3k0813@gmail.com>
2025-06-21 11:17:22 +0900

    chore: 안쓰는 메서드 제거

--- src/main/java/com/gotogether/domain/ticketoptionanswer/service/TicketOptionAnswerService.java
+++ src/main/java/com/gotogether/domain/ticketoptionanswer/service/TicketOptionAnswerService.java
@@ -3,18 +3,14 @@ package com.gotogether.domain.ticketoptionanswer.service;
 import java.util.List;
 
 import com.gotogether.domain.order.entity.Order;
-import com.gotogether.domain.ticket.entity.Ticket;
 import com.gotogether.domain.ticketoptionanswer.dto.request.TicketOptionAnswerRequestDTO;
 import com.gotogether.domain.ticketoptionanswer.dto.response.PurchaserAnswerDetailResponseDTO;
 import com.gotogether.domain.ticketoptionanswer.dto.response.PurchaserAnswerListResponseDTO;
-import com.gotogether.domain.ticketoptionanswer.entity.TicketOptionAnswer;
 import com.gotogether.domain.user.entity.User;
 
 public interface TicketOptionAnswerService {
 
 	void createTicketOptionAnswer(Long userId, TicketOptionAnswerRequestDTO request);
-
-	List<TicketOptionAnswer> getPendingAnswersByTicket(Ticket ticket);
 
 	List<PurchaserAnswerDetailResponseDTO> getAnswersByTicket(Long ticketId);
 
