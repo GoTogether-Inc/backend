3348fba173ffd98f294d7fdfa1a22ed3005ee97d
JJM <m3k0813@gmail.com>
2025-07-09 14:08:47 +0900

    fix: 주문 생성 시 티켓 데이터에 비관적 락 적용

--- src/main/java/com/gotogether/domain/ticket/repository/TicketRepository.java
+++ src/main/java/com/gotogether/domain/ticket/repository/TicketRepository.java
@@ -1,13 +1,23 @@
 package com.gotogether.domain.ticket.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.gotogether.domain.ticket.entity.Ticket;
 
+import jakarta.persistence.LockModeType;
+
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
+
 	List<Ticket> findByEventId(Long eventId);
+
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
+	@Query("SELECT t FROM Ticket t WHERE t.id = :id")
+	Optional<Ticket> findByIdWithPessimisticLock(Long id);
 }
