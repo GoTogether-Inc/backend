e8c78de44cb46f59f6e087cf319bd16cc8f0cbb1
희은 <datakubit2@gmail.com>
2025-07-10 15:46:21 +0900

    refactor: CachingRequestResponseFilter에서 요청 캐싱 임시 제거

--- src/main/java/com/gotogether/global/filter/CachingRequestResponseFilter.java
+++ src/main/java/com/gotogether/global/filter/CachingRequestResponseFilter.java
@@ -8,20 +8,21 @@ import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
-import org.springframework.web.util.ContentCachingRequestWrapper;
 import org.springframework.web.util.ContentCachingResponseWrapper;
 
 import java.io.IOException;
 
+// TODO: 요청 바디 로깅 필터 추가
 @Component
 public class CachingRequestResponseFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        ContentCachingRequestWrapper cachingRequest = new ContentCachingRequestWrapper((HttpServletRequest) request);
-        ContentCachingResponseWrapper cachingResponse = new ContentCachingResponseWrapper((HttpServletResponse) response);
-
-        chain.doFilter(cachingRequest, cachingResponse);
-
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        
+        ContentCachingResponseWrapper cachingResponse = new ContentCachingResponseWrapper(httpResponse);
+        
+        chain.doFilter(httpRequest, cachingResponse);
         cachingResponse.copyBodyToResponse();
     }
 }
