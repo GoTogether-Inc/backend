62a81e4fa93db70b362d336ecfd47420220fdfc3
Jeongjun Min <m3k0813@gmail.com>
2026-02-08 21:01:36 +0900

    chore: S3에서 MinIO로 설정 변경

--- src/main/java/com/gotogether/global/config/AwsS3Config.java
+++ src/main/java/com/gotogether/global/config/AwsS3Config.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
@@ -20,6 +21,12 @@ public class AwsS3Config {
 	@Value("${amazon.aws.region}")
 	private String s3RegionName;
 
+	@Value("${amazon.aws.endpoint}")
+	private String s3Endpoint;
+
+	@Value("${amazon.aws.path-style-access}")
+	private boolean s3PathStyleAccess;
+
 	@Bean
 	public AmazonS3 getAmazonS3Client() {
 		final BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(accessKeyId, accessKeySecret);
@@ -27,7 +34,8 @@ public class AwsS3Config {
 		return AmazonS3ClientBuilder
 			.standard()
 			.withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials))
-			.withRegion(s3RegionName)
+			.withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(s3Endpoint, s3RegionName))
+			.withPathStyleAccessEnabled(s3PathStyleAccess)
 			.build();
 	}
 }
